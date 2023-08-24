ddbccb40c6d5654cfdd474babfe17decbcd6a652
Aleksei Punov <design.qs@gmail.com>
2023-08-23 22:56:51 -0700

    Downgrade OpenCV version for compatibility

--- dashcam/package/python-opencv-python/python-opencv-python.mk
+++ dashcam/package/python-opencv-python/python-opencv-python.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OPENCV_PYTHON_VERSION = 4.8.0.76
+PYTHON_OPENCV_PYTHON_VERSION = 4.5.5.64
 PYTHON_OPENCV_PYTHON_SOURCE = opencv-python-$(PYTHON_OPENCV_PYTHON_VERSION).tar.gz
 PYTHON_OPENCV_PYTHON_SITE = https://files.pythonhosted.org/packages/32/72/03747a6820bc970aeb0b89e653d1084068ac1ed606a83d8b5ac6fc237c14
 PYTHON_OPENCV_PYTHON_SETUP_TYPE = setuptools
