ca76dbb1839414eafb36a87a2b224a89bc625df9
Aleksei Punov <design.qs@gmail.com>
2024-02-08 14:26:15 -0800

    3.9.5 with skip filtering

--- dashcam/package/camera-node/files/dashcam-api.js
+++ dashcam/package/camera-node/files/dashcam-api.js
@@ -120,7 +120,7 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.isDev = exports.API_VERSION = void 0;
 __exportStar(__nccwpck_require__(9020), exports);
-exports.API_VERSION = '3.9.4';
+exports.API_VERSION = '3.9.5';
 const isDev = () => {
     return false;
 };
@@ -64676,7 +64676,7 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.isDev = exports.API_VERSION = void 0;
 __exportStar(__nccwpck_require__(4484), exports);
-exports.API_VERSION = '3.9.4';
+exports.API_VERSION = '3.9.5';
 const isDev = () => {
     return false;
 };
