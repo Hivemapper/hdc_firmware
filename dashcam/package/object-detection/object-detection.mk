9aa6b9c25847a953f3ce5317c120b9a0eebe42c0
Aleksei Punov <design.qs@gmail.com>
2024-02-15 18:06:31 -0800

    ODC API v4.2 with service files and library updates

---
+++ dashcam/package/object-detection/object-detection.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# object-detection
+#
+################################################################################
+
+OBJECT_DETECTION_VERSION = 1.0.0
+OBJECT_DETECTION_SITE = $(BR2_EXTERNAL_DASHCAM_PATH)/package/object-detection/files
+OBJECT_DETECTION_SITE_METHOD = local
+
+define OBJECT_DETECTION_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 644 $(@D)/detect_hdc.py \
+		$(TARGET_DIR)/opt/dashcam/bin/detect_hdc.py
+	$(INSTALL) -D -m 644 $(@D)/image.py \
+		$(TARGET_DIR)/opt/dashcam/bin/image.py
+	$(INSTALL) -D -m 644 $(@D)/sqlite.py \
+		$(TARGET_DIR)/opt/dashcam/bin/sqlite.py
+	$(INSTALL) -D -m 644 $(@D)/model.tflite \
+		$(TARGET_DIR)/opt/dashcam/bin/model.tflite
+	$(INSTALL) -D -m 644 $(@D)/object-detection.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/object-detection.service
+endef
+
+$(eval $(generic-package))
