153c8eacb985a740ba3bf52c5364059051216c41
Aleksei Punov <design.qs@gmail.com>
2024-02-26 15:57:45 -0800

    Version update

--- dashcam/package/camera-node/camera-node.mk
+++ dashcam/package/camera-node/camera-node.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CAMERA_NODE_VERSION = 4.2.4
+CAMERA_NODE_VERSION = 4.2.5
 CAMERA_NODE_SITE = $(BR2_EXTERNAL_DASHCAM_PATH)/package/camera-node/files
 CAMERA_NODE_SITE_METHOD = local
 CAMERA_NODE_DEPENDENCIES = nodejs
