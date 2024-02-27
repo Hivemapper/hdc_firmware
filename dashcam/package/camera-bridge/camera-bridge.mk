a5d4eba693358646e990c857b97d4332d7553d03
Aleksei Punov <design.qs@gmail.com>
2024-02-27 15:55:36 -0800

    More original code

--- dashcam/package/camera-bridge/camera-bridge.mk
+++ dashcam/package/camera-bridge/camera-bridge.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CAMERA_BRIDGE_VERSION = 0f4483873668942da02ee496b0fb0bfe8381942d
+CAMERA_BRIDGE_VERSION = dbaea578be73fca997c9f068568c5c9691abcee1
 CAMERA_BRIDGE_SITE = git@github.com:Hivemapper/camera_bridge.git
 CAMERA_BRIDGE_SITE_METHOD = git
 CAMERA_BRIDGE_CONF_OPTS = -DENABLE_OPENCV=0 -DCMAKE_INSTALL_PREFIX="/opt/dashcam"\
