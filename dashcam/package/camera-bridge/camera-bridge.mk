e12e173165a3779272a2e9992e4979cd97430554
Aleksei Punov <design.qs@gmail.com>
2024-02-29 12:25:03 -0800

    Camera-bridge with queue control

--- dashcam/package/camera-bridge/camera-bridge.mk
+++ dashcam/package/camera-bridge/camera-bridge.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CAMERA_BRIDGE_VERSION = 6926963fefe26b43467bd54960bb7b695ddbfc64
+CAMERA_BRIDGE_VERSION = c4d3855d33e482cea84d20f09a1c1c3e40f587bb
 CAMERA_BRIDGE_SITE = git@github.com:Hivemapper/camera_bridge.git
 CAMERA_BRIDGE_SITE_METHOD = git
 CAMERA_BRIDGE_CONF_OPTS = -DENABLE_OPENCV=0 -DCMAKE_INSTALL_PREFIX="/opt/dashcam"\
