8f887815e763636183951e20f116a5eba60893e6
Aleksei Punov <design.qs@gmail.com>
2024-02-28 18:31:07 -0800

    BufferPool experimentals

--- dashcam/package/camera-bridge/camera-bridge.mk
+++ dashcam/package/camera-bridge/camera-bridge.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CAMERA_BRIDGE_VERSION = 7982878e21414af2a9e52debcb3b72777e2c4ba6
+CAMERA_BRIDGE_VERSION = c11e6a505730214830c11fe4e046958691b9d314
 CAMERA_BRIDGE_SITE = git@github.com:Hivemapper/camera_bridge.git
 CAMERA_BRIDGE_SITE_METHOD = git
 CAMERA_BRIDGE_CONF_OPTS = -DENABLE_OPENCV=0 -DCMAKE_INSTALL_PREFIX="/opt/dashcam"\
