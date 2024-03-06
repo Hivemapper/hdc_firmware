6e89e3a4b7852bdd20651e2b051af6c3baf30758
Aleksei Punov <design.qs@gmail.com>
2024-03-06 11:35:53 -0800

    ODC API v4.2.11 n800 models for testing

--- dashcam/package/object-detection/object-detection.mk
+++ dashcam/package/object-detection/object-detection.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OBJECT_DETECTION_VERSION = 4.2.10.1
+OBJECT_DETECTION_VERSION = 4.2.11
 OBJECT_DETECTION_SITE = $(BR2_EXTERNAL_DASHCAM_PATH)/package/object-detection/files
 OBJECT_DETECTION_SITE_METHOD = local
 
@@ -15,12 +15,10 @@ define OBJECT_DETECTION_INSTALL_TARGET_CMDS
 		$(TARGET_DIR)/opt/dashcam/bin/image.py
 	$(INSTALL) -D -m 644 $(@D)/sqlite.py \
 		$(TARGET_DIR)/opt/dashcam/bin/sqlite.py
-	$(INSTALL) -D -m 644 $(@D)/model.tflite \
-		$(TARGET_DIR)/opt/dashcam/bin/model.tflite
-	$(INSTALL) -D -m 644 $(@D)/n640_float16.tflite \
-		$(TARGET_DIR)/opt/dashcam/bin/n640_float16.tflite
-	$(INSTALL) -D -m 644 $(@D)/n640_2x2_float16.tflite \
-		$(TARGET_DIR)/opt/dashcam/bin/n640_2x2_float16.tflite
+	$(INSTALL) -D -m 644 $(@D)/n800_1x2_float16.tflite \
+		$(TARGET_DIR)/opt/dashcam/bin/n800_1x2_float16.tflite
+	$(INSTALL) -D -m 644 $(@D)/n800_2x2_float16.tflite \
+		$(TARGET_DIR)/opt/dashcam/bin/n800_2x2_float16.tflite
 	$(INSTALL) -D -m 644 $(@D)/object-detection.service \
 		$(TARGET_DIR)/usr/lib/systemd/system/object-detection.service
 endef
