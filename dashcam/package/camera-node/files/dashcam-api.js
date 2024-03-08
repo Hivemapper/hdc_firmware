635bdf45071176ca833e09c0bbf201b12c5a0a62
Aleksei Punov <design.qs@gmail.com>
2024-03-07 18:39:03 -0800

    Revert "ODC API v4.4.11 - int quant"
    This reverts commit 2fbfed4b00cf0999f889a8e71c6cb3d2025402b1.

--- dashcam/package/camera-node/files/dashcam-api.js
+++ dashcam/package/camera-node/files/dashcam-api.js
@@ -121,7 +121,7 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.isDev = exports.API_VERSION = void 0;
 __exportStar(__nccwpck_require__(9020), exports);
-exports.API_VERSION = '4.4.11';
+exports.API_VERSION = '4.4.10';
 const isDev = () => {
     return false;
 };
@@ -64766,7 +64766,7 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.isDev = exports.API_VERSION = void 0;
 __exportStar(__nccwpck_require__(4484), exports);
-exports.API_VERSION = '4.4.11';
+exports.API_VERSION = '4.4.10';
 const isDev = () => {
     return false;
 };
