342eca10b1c59454f6fb9eea9fdc61c432effa56
Aleksei Punov <design.qs@gmail.com>
2024-03-12 20:29:57 -0700

    ODC API v4.4.17

--- dashcam/package/camera-node/camera-node.mk
+++ dashcam/package/camera-node/camera-node.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CAMERA_NODE_VERSION = 4.4.16
+CAMERA_NODE_VERSION = 4.4.17
 CAMERA_NODE_SITE = $(BR2_EXTERNAL_DASHCAM_PATH)/package/camera-node/files
 CAMERA_NODE_SITE_METHOD = local
 CAMERA_NODE_DEPENDENCIES = nodejs
