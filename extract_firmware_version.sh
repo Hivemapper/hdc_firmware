f7526f72e8aaf53704b8cd5ccfed6ce44291853a
Mazar Farran <mazar@hivemapper.com>
2024-05-29 18:18:03 -0700

    Fix

--- extract_firmware_version.sh
+++ extract_firmware_version.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-camera_node_dir=$(find output/build -maxdepth 1 -type d -name 'camera-node-*')
+camera_node_dir=$(find ../output/build -maxdepth 1 -type d -name 'camera-node-*')
 echo $camera_node_dir
 dashcam_api_path=$camera_node_dir/compiled/odc-api-hdc.js
 
 # Extract the firmware version from the odc-api script and transform it to X_Y_Z format
-grep -m 1 "exports.API_VERSION = '" $dashcam_api_path | sed -n "s/.*'\(.*\)'.*/\1/p" | sed 's/\([0-9]\+\)\.\([0-9]\+\)\.\([0-9]\+\)/\1_\2_\3/g'
+grep -m 1 "exports.API_VERSION = '" $dashcam_api_path | sed -n "s/.*'\(.*\)'.*/\1/p" | sed 's/\([0-9]\+\)\.\([0-9]\+\)\.\([0-9]\+\)/\1-\2-\3/g'
