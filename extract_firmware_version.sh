587d4c943a2ed9e347060f7dcdd2ab99d00bd8f4
mazar-farran <150729175+mazar-farran@users.noreply.github.com>
2024-06-11 14:13:08 -0700

    Update github workflows for building and uploading HDC Firmware. (#254)
    Ticket: https://github.com/Hivemapper/hdc_firmware/issues/212
    
    compile_hdc workflow compiles the project.
    deploy_s3 workflow uploads to the cicd folder in the dashcam-firmware bucket. This was intentional to prevent accidentally overwriting an existing release. The user must manually move the new firmware from cicd to the root directory. 
    'clean' and 'super clean' workflows clean the workspace

---
+++ extract_firmware_version.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+camera_node_dir=$(find ../output/build -maxdepth 1 -type d -name 'camera-node-*')
+dashcam_api_path=$camera_node_dir/compiled/odc-api-hdc.js
+# Extract the firmware version from the odc-api script and transform it to X_Y_Z format
+grep -m 1 "exports.API_VERSION = '" $dashcam_api_path | sed -n "s/.*'\(.*\)'.*/\1/p" | sed 's/\([0-9]\+\)\.\([0-9]\+\)\.\([0-9]\+\)/\1-\2-\3/g'
