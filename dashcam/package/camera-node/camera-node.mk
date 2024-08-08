1cc2a057833077b66ee9983dc394b170ed18dc5b
mazar-farran <150729175+mazar-farran@users.noreply.github.com>
2024-08-08 09:58:28 -0700

    Restore USB circular writes and fix images not getting deleted in order. (#258)
    Ticket: https://linear.app/hivemapper/issue/IMCO-166/investigate-hdc-recording-issue
    
    Check for gnss date and use it as an offset if it's available.
    
    Related to: Hivemapper/odc-api#221

--- dashcam/package/camera-node/camera-node.mk
+++ dashcam/package/camera-node/camera-node.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-CAMERA_NODE_VERSION = 30dc42c03679119765211929d4c7fe56e4c18306
-CAMERA_NODE_SITE = git@github.com:Hivemapper/odc-api.git
+CAMERA_NODE_VERSION = 75b20d40c3b1c074e37cc894ee7eb18474097a20
+CAMERA_NODE_SITE = https://github.com/Hivemapper/odc-api.git
 CAMERA_NODE_SITE_METHOD = git
 CAMERA_NODE_DEPENDENCIES = host-nodejs nodejs
 
