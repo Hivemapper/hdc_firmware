eb88bd0b0a719341228f581f0728fc997c44f6b7
mazar-farran <mazar@hivemapper.com>
2024-08-15 23:32:34 +0000

    Restore data logger build from source

--- dashcam/package/data-logger/data-logger.mk
+++ dashcam/package/data-logger/data-logger.mk
@@ -4,13 +4,34 @@
 #
 ################################################################################
 
-DATA_LOGGER_VERSION = 1.5.8
-DATA_LOGGER_SITE = https://github.com/Hivemapper/hivemapper-data-logger/releases/download/v$(DATA_LOGGER_VERSION)
-DATA_LOGGER_SOURCE = hivemapper-data-logger_$(DATA_LOGGER_VERSION)_Linux_arm64.tar.gz
-DATA_LOGGER_STRIP_COMPONENTS = 0
+DATA_LOGGER_VERSION = a51e746b0ddf5bc8ec0fe2342a743f466a06568d # Release 1.5.8
+DATA_LOGGER_SITE = https://github.com/Hivemapper/hivemapper-data-logger.git
+DATA_LOGGER_SITE_METHOD = git
+DATA_LOGGER_GOLANG_BUILD_TARGETS += ./cmd/datalogger
+DATA_LOGGER_GOLANG_INSTALL_BINS += datalogger
+DATA_LOGGER_GOMOD = ./cmd/datalogger
+
+DATA_LOGGER_PRE_BUILD_HOOKS += DATA_LOGGER_CUSTOM_PRE_BUILD
+
+
+# Fixes the go.mod to point to the correct gnss-controller commit
+# https://github.com/Hivemapper/gnss-controller/pull/4/commits/01235228b063afaf3e1961008454c959ad6c6044
+# This is a temporary solution until we merge the above PR to main
+# and have an agreed upon stable branch for HDC.
+define DATA_LOGGER_CUSTOM_PRE_BUILD
+    echo "Running custom pre-build steps for golang-package"
+	cd $(@D) && \
+	{\
+		pwd; \
+		sed -i 's/replace github\.com\/Hivemapper\/gnss-controller.*//g' go.mod; \
+		sed -i 's/v1\.0\.3-0\.20240402232423-1de9f3a3a7f8/v1\.0\.3-0\.20240514192901-01235228b063/g' go.mod; \
+		$(GO_BIN) get github.com/Hivemapper/hivemapper-data-logger/cmd/datalogger; \
+		$(GO_BIN) mod vendor; \
+	}
+endef
 
 define DATA_LOGGER_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 755 $(@D)/datalogger $(TARGET_DIR)/opt/dashcam/bin
+	$(INSTALL) -D -m 755 $(@D)/bin/data-logger $(TARGET_DIR)/opt/dashcam/bin/datalogger
 endef
 
 define DATA_LOGGER_INSTALL_INIT_SYSTEMD
@@ -18,4 +39,4 @@ define DATA_LOGGER_INSTALL_INIT_SYSTEMD
 		$(TARGET_DIR)/usr/lib/systemd/system/data-logger.service
 endef
 
-$(eval $(generic-package))
+$(eval $(golang-package))
