1259c65d35ac103c7fc419b0125e937922fd70f5
mazar-farran <mazar@hivemapper.com>
2024-08-16 07:50:40 +0000

    Fix

--- dashcam/package/data-logger/data-logger.mk
+++ dashcam/package/data-logger/data-logger.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DATA_LOGGER_VERSION = a51e746b0ddf5bc8ec0fe2342a743f466a06568d # Release 1.5.8
+DATA_LOGGER_VERSION = d153cff3df198613b89574a385a24e369a9c533c
 DATA_LOGGER_SITE = https://github.com/Hivemapper/hivemapper-data-logger.git
 DATA_LOGGER_SITE_METHOD = git
 DATA_LOGGER_GOLANG_BUILD_TARGETS += ./cmd/datalogger
@@ -12,23 +12,6 @@ DATA_LOGGER_GOLANG_INSTALL_BINS += datalogger
 DATA_LOGGER_GOMOD = ./cmd/datalogger
 
 DATA_LOGGER_PRE_BUILD_HOOKS += DATA_LOGGER_CUSTOM_PRE_BUILD
-
-
-# Fixes the go.mod to point to the correct gnss-controller commit
-# https://github.com/Hivemapper/gnss-controller/pull/4/commits/01235228b063afaf3e1961008454c959ad6c6044
-# This is a temporary solution until we merge the above PR to main
-# and have an agreed upon stable branch for HDC.
-define DATA_LOGGER_CUSTOM_PRE_BUILD
-    echo "Running custom pre-build steps for golang-package"
-	cd $(@D) && \
-	{\
-		pwd; \
-		sed -i 's/replace github\.com\/Hivemapper\/gnss-controller.*//g' go.mod; \
-		sed -i 's/v1\.0\.3-0\.20240402232423-1de9f3a3a7f8/v1\.0\.3-0\.20240514192901-01235228b063/g' go.mod; \
-		$(GO_BIN) get github.com/Hivemapper/hivemapper-data-logger/cmd/datalogger; \
-		$(GO_BIN) mod vendor; \
-	}
-endef
 
 define DATA_LOGGER_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 755 $(@D)/bin/data-logger $(TARGET_DIR)/opt/dashcam/bin/datalogger
